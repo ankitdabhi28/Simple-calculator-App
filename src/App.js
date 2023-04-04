c115373ffc0ccea532a40e68db638d6fbb7b616c
ankitdabhi28 <dabhiankit9033@gmail.com>
2023-04-04 13:15:21 +0530

    _

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="main">
       <Provider store={store}>
         <DisplayData />
-        <Buttons />
+        <Buttons />   
       </Provider>
     </div>
     );
